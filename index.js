5be9daf32b65f84fafe3ef5e0a8e173d2bae6c0b
Ronnie Nygren <r.k.n.post@gmail.com>
2020-08-16 12:42:10 +0300

    Action code

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const shell = require('shelljs');
+
+try {
+  const projectTotransform = core.getInput('project-to-transformt');
+  const projectsDir = core.getInput('projects-dir');
+  // Here's what we want to do:
+  // Remove everything from root level
+  // Move everything from projects dir to root level
+  // Remove projects dir
+    shell.mv(`./${projectsDir}/${projectTotransform}`, '/tmp');
+    shell.rm('-rf', './*');
+    shell.mv(`/tmp/${projectTotransform}/*`, './');
+} catch (error) {
+  core.setFailed(error.message);
+}
