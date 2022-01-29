98222e6798b3d9574313019ff6fc80ba3ca1fc48
Ronnie Nygren <ronnie@rnygren.com>
2022-01-29 18:38:33 +0200

    Preserve lock files when transforming

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ try {
     console.log(shell.exec('ls .'));
     console.log(projectTotransform, projectsDir);
     shell.mv(`./${projectsDir}/${projectTotransform}`, '/tmp');
+    // This is a special file, only one lock file exists and it's in root
+    shell.mv(`./yarn.lock`, `/tmp/${projectTotransform}`);
     shell.rm('-rf', './*');
     shell.mv(`/tmp/${projectTotransform}/*`, './');
 } catch (error) {
